18de8cc1ac6b88cd5a406359ae4eb2a80a38d2f0
somi9954 <jolh999@gmail.com>
2023-10-01 19:04:20 +0900

    사이트 기본 설정 중

---
+++ src/main/java/org/project/configs/MvcConfig.java
@@ -0,0 +1,16 @@
+package org.project.configs;
+
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties(FileConfig.class)
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
