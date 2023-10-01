3a91f0938ccb8763fa878fed4059ce12b90ed3ac
somi9954 <jolh999@gmail.com>
2023-10-02 00:47:18 +0900

    채팅 기록 구현중

--- src/main/java/org/project/controllers/chat/ChatRoomForm.java
+++ src/main/java/org/project/controllers/chat/ChatRoomForm.java
@@ -1,7 +1,11 @@
 package org.project.controllers.chat;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 //getter, 생성자 매개변수 , toString, equalsAndHashCode
-public record ChatRoomForm(Long roomNo, String roomNm, int max) { }
+public record ChatRoomForm(
+        Long roomNo,
+        @NotBlank String roomNm,
+        int max) { }
