09a1f86e109e622ff7d3cfc81ab97774ac605ca8
somi9954 <jolh999@gmail.com>
2023-10-10 22:50:56 +0900

    프론트 앱 구성

---
+++ front_app/chatting_fe/src/pages/Login.js
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const isLogin = true;
+  const navigate = useNavigate();
+  if (isLogin) {
+    navigate("/");
+    
+  }
+  return <h1>로그인</h1>
+};
+
+export default Login;
---
+++ front_app/chatting_fe/src/pages/join.js
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+
+const Join = () => {
+  const isLogin = true;
+  if(isLogin) {
+    return <Navigate to="/" replace={true} />
+  }
+
+  return <h1>회원가입</h1>
+};
+
+export default Join;
