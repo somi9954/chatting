2e91eb6affc00d52050e014172bcaf9dddece3d8
somi9954 <jolh999@gmail.com>
2023-10-22 16:14:34 +0900

    .

--- src/main/java/org/project/entities/ChatRoom.java
+++ src/main/java/org/project/entities/ChatRoom.java
@@ -1,26 +1,25 @@
 package org.project.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
-@Data
-@Builder
+@Data @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class ChatRoom extends BaseEntity {
     @Id @GeneratedValue
     private Long roomNo;
-    @Column(length = 60, nullable = false)
+    @Column(length=60, nullable = false)
     private String roomNm;
-    private int max; //최대 인원수 한정, 0이면 무제한
+    private int max; // 최대 인원수, 0 이면 무제한
 
-    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY)
-    private List<ChatHistory> histories = new ArrayList<>();
-
+    // @ToString.Exclude
+    // @OneToMany(mappedBy = "room", fetch=FetchType.LAZY)
+    // private List<ChatHistory> histories = new ArrayList<>();
 }
